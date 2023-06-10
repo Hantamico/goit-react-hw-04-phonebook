2e0682503f86eace90c70821154a51d6d72cfa60
Kirill_Halahan <Galagan.k.a.01@gmail.com>
2023-06-10 15:40:52 +0300

    hw 4 Phonebook Hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,81 +1,62 @@
-import React, { Component } from "react";
+import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
-
-import { Form } from "./Form/Form";
+import Form from "./Form/Form";
 import Contacts from "./Contacts/Contacts";
 import Filter from "./Filter/Filter";
 
-class App extends Component{
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter:'',
-  };
+const useLocalStorage = (key, defaultValue) => {
+  const [state, setState] = useState(() => {
+    return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
+  });
 
-  componentDidMount() {
-    const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state])
 
-    if (parsedContacts) {
-      this.setState({contacts: parsedContacts})
-    } 
-  };
+  return [state, setState];
+}
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-    }
-  }
+export default function App() {
+  const [contacts, setContacts] = useLocalStorage('contacts', [])
 
-  formSubmitHandler = ({ name, number }) => {
-    if (this.state.contacts.some(contact => contact.name === name)) {
+  const [filter, setFilter] = useState('');
+
+
+  function formSubmitHandler(name, number) {
+    if (contacts.some(contact => contact.name === name)) {
       alert(`${name} is already in contacts.`);
       return;
     }
 
-    console.log({ name, number });
-    
     const newContact = {
       id: nanoid(),
       name: name,
       number: number,
     }
 
-    this.setState({contacts: [...this.state.contacts, newContact]})
+    setContacts(v => [...v, newContact]);
   };
 
-  deleteContact = (contactId) => {
-    console.log(contactId)
-    this.setState({contacts: this.state.contacts.filter(contact => contactId !== contact.id)})
+  function handleFilterChange(e) {
+    setFilter(e.currentTarget.value);
   };
 
-  handleFilterChange = evt => {
-    this.setState({filter: evt.target.value});
+  function deleteContact(contactId) {
+    setContacts(contacts.filter(contact => contactId !== contact.id))
   };
 
-
-
-  render() {
-    const contacts = this.state.contacts;
-    
-    const filteredContacts = contacts.filter(contact => {
-      return contact.name.toLowerCase().includes(this.state.filter.toLowerCase());
+  const filteredContacts = contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(filter.toLowerCase());
     })
-
-    return (
+  
+  return (
       <div>
         <h1>Phonebook</h1>
-        <Form onSubmit={this.formSubmitHandler} />
+        <Form onSubmit={formSubmitHandler} />
         <h2>Contacts</h2>
-        <Filter filter={this.state.filter} onChange={this.handleFilterChange}/>
-        <Contacts contacts={filteredContacts} handleDelete={this.deleteContact} />
+        <Filter filter={filter} onChange={handleFilterChange}/>
+        <Contacts contacts={filteredContacts} handleDelete={deleteContact} />
       </div>
       
     );
-  };
-}
-
-export default App;
+};
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,29 +1,33 @@
-import React, {Component} from "react";
+import {useState} from "react";
 import css from './form.module.css'
+import PropTypes from 'prop-types';
 
-export class Form extends Component{
-    state = {
-        name: '',
-        number: '',
+export default function Form({onSubmit}) {
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
+
+    function handleChange(e) {
+        switch (e.currentTarget.name) {
+            case 'name':
+                setName(e.currentTarget.value);
+                break;
+            case 'number':
+                setNumber(e.currentTarget.value);
+                break;
+            default:
+                return;
+        }
     }
 
-     handleChange = evt => {
-        this.setState({[evt.currentTarget.name]: evt.currentTarget.value})
-     }
-    
-    handleSubmit = evt => {
-        evt.preventDefault();
-        this.props.onSubmit(this.state);
-        this.reset();
+    function handleSubmit(e) {
+        e.preventDefault();
+        onSubmit(name, number);
+        setName('');
+        setNumber('');
     }
     
-    reset = () => {
-        this.setState({name: '', number: '',})
-    }
-    
-    render(){
-        return (
-            <form className={css.form} onSubmit={this.handleSubmit}>
+    return (
+            <form className={css.form} onSubmit={handleSubmit}>
                 <label>
                     Name
                     <input
@@ -33,8 +37,8 @@ export class Form extends Component{
                         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                         required
-                        value={this.state.name}
-                        onChange={this.handleChange}
+                        value={name}
+                        onChange={handleChange}
                     />
                 </label>
                 <label>
@@ -45,13 +49,16 @@ export class Form extends Component{
                         name="number"
                         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                        value={this.state.number}
-                        onChange={this.handleChange}
+                        value={number}
+                        onChange={handleChange}
                         required
                     />
                 </label>
                 <button className={css.btn__submit}>Add Contact</button>
             </form>
         )
-    }
+}
+
+Form.propTypes = {
+    onSubmit: PropTypes.func,
 }
